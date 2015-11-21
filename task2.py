bededa98ceb17b786eb6dbaabe562156e3241a00
tarunlnmiit <tarungupta.y12@lnmiit.ac.in>
2015-11-21 21:47:40 +0530

    first commit

---
+++ task2.py
@@ -0,0 +1,84 @@
+from random import shuffle
+
+punc_list = [".",",",";",":","!","'",'"',"-"]
+
+# Input string/strings
+print '***************Enter data**************** '
+s = ""
+while True:
+	temp = raw_input() + "\n"
+	if temp == "\n":
+		break
+	s += temp
+
+# Opening a file to write the input sentence/sentences or paragraphs
+
+fhand = open("inputData.txt", 'w')
+fhand.write(s)
+fhand.close()
+
+# Reading the input data line by line 
+fhand = open("inputData.txt", 'r')
+
+# Writing the data after scrambling
+fhand1 = open('data.txt', 'w')
+
+# Function to perform scrambling
+for line in fhand:
+	s = line.rstrip().split()
+	
+	for i in xrange(len(s)):
+		
+		if s[i].isalpha():
+			scramble_text = list(s[i][1:len(s[i])-1])
+			shuffle(scramble_text)
+			scramble_text = "".join(scramble_text)
+			
+			if len(s[i]) > 1:
+				s[i] = s[i][0] + scramble_text + s[i][-1]
+			
+			else:
+	 			s[i] = s[i][0] + scramble_text
+	 	
+	 	else:
+	 		
+	 		for item in punc_list:
+	 		
+	 			if item in s[i]:
+	 				s1, s2 = s[i].split(item)
+	 		
+	 				if len(s2) == 0:
+	 					scramble_text = list(s1[1:len(s1)-1])
+	 					shuffle(scramble_text)
+	 					scramble_text = "".join(scramble_text)
+	 		
+	 					if len(s1) > 1:
+	 						s1 = s1[0] + scramble_text + s1[-1]
+	 		
+	 					else:
+	 						s1 = s1[0] + scramble_text
+	 					s[i] = s1 + item + s2
+	 		
+	 				else:
+	 					scramble_text = list(s1[1:len(s1)])
+	 					shuffle(scramble_text)
+	 					scramble_text = "".join(scramble_text)
+	 					s1 = s1[0] + scramble_text
+	 					scramble_text = list(s2[:-1])
+	 					shuffle(scramble_text)
+	 					scramble_text = "".join(scramble_text)
+	 					s2 = scramble_text + s2[-1]
+	 					s[i] = s1 + item + s2
+	
+	s = " ".join(s)
+	fhand1.write(s+  '\n')
+
+fhand1.close()
+fhand.close()
+
+fhand = open('data.txt', 'r')
+
+for line in fhand:
+	print line.rstrip()
+
+fhand.close()

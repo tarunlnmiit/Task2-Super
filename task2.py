35ae123f9fc986176397ec228bc6f8b7ec9e27d8
tarunlnmiit <tarungupta.y12@lnmiit.ac.in>
2015-11-22 01:35:23 +0530

    second commit

--- task2.py
+++ task2.py
@@ -29,6 +29,7 @@ for line in fhand:
 	
 	for i in xrange(len(s)):
 		
+		# Code for words with no punctuation 
 		if s[i].isalpha():
 			scramble_text = list(s[i][1:len(s[i])-1])
 			shuffle(scramble_text)
@@ -41,34 +42,41 @@ for line in fhand:
 	 			s[i] = s[i][0] + scramble_text
 	 	
 	 	else:
-	 		
-	 		for item in punc_list:
-	 		
-	 			if item in s[i]:
-	 				s1, s2 = s[i].split(item)
-	 		
-	 				if len(s2) == 0:
-	 					scramble_text = list(s1[1:len(s1)-1])
-	 					shuffle(scramble_text)
-	 					scramble_text = "".join(scramble_text)
-	 		
-	 					if len(s1) > 1:
-	 						s1 = s1[0] + scramble_text + s1[-1]
-	 		
-	 					else:
-	 						s1 = s1[0] + scramble_text
-	 					s[i] = s1 + item + s2
-	 		
-	 				else:
-	 					scramble_text = list(s1[1:len(s1)])
-	 					shuffle(scramble_text)
-	 					scramble_text = "".join(scramble_text)
-	 					s1 = s1[0] + scramble_text
-	 					scramble_text = list(s2[:-1])
-	 					shuffle(scramble_text)
-	 					scramble_text = "".join(scramble_text)
-	 					s2 = scramble_text + s2[-1]
-	 					s[i] = s1 + item + s2
+	 		# For words which have same punctuation at start and end
+	 		if s[i][0] == s[i][-1] and s[i][0] in punc_list and s[i][-1] in punc_list:
+	 			scramble_text = list(s[i][2:len(s[i])-2])
+		 		shuffle(scramble_text)
+		 		scramble_text = "".join(scramble_text)
+		 		s[i] = s[i][:2] + scramble_text	+ s[i][-2:]
+		 	
+	 		else:
+		 			for item in punc_list:
+		 		
+			 			if item in s[i]:
+			 				s1, s2 = s[i].split(item)
+			 		
+			 				if len(s2) == 0:
+			 					scramble_text = list(s1[1:len(s1)-1])
+			 					shuffle(scramble_text)
+			 					scramble_text = "".join(scramble_text)
+			 		
+			 					if len(s1) > 1:
+			 						s1 = s1[0] + scramble_text + s1[-1]
+			 		
+			 					else:
+			 						s1 = s1[0] + scramble_text
+			 					s[i] = s1 + item + s2
+			 		
+			 				else:
+			 					scramble_text = list(s1[1:len(s1)])
+			 					shuffle(scramble_text)
+			 					scramble_text = "".join(scramble_text)
+			 					s1 = s1[0] + scramble_text
+			 					scramble_text = list(s2[:-1])
+			 					shuffle(scramble_text)
+			 					scramble_text = "".join(scramble_text)
+			 					s2 = scramble_text + s2[-1]
+			 					s[i] = s1 + item + s2
 	
 	s = " ".join(s)
 	fhand1.write(s+  '\n')
